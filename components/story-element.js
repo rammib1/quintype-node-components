7327541ec7f93a25af713da99beccdead3e2b80f
ajaymenon0 <ajaymenon0@gmail.com>
2018-04-30 10:43:04 +0530

    Have story prop in renderTemplate

--- components/story-element.js
+++ components/story-element.js
@@ -98,6 +98,7 @@ class StoryElementBase extends React.Component {
 
   render() {
     const storyElement = this.props.element;
+    const storyObj = this.props.story;
     const typeClassName = `story-element-${storyElement.type}`;
     const subtypeClassName = `story-element-${storyElement.type}-${storyElement.subtype}`;
 
@@ -115,7 +116,7 @@ class StoryElementBase extends React.Component {
     }, (renderTemplate?
       React.createElement(
         renderTemplate,
-        Object.assign({}, {element: storyElement}),
+        Object.assign({}, {element: storyElement, story: storyObj}),
         React.createElement(storyElementTemplate, Object.assign({}, elementProps))
       ) : React.createElement(storyElementTemplate, Object.assign({}, elementProps)))
     )
