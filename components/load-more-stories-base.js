c0b60c5f214c48d531a2d205e9b0212bff179161
Garima Kamboj <garima.kamboj@icloud.com>
2018-05-30 18:36:05 +0530

    Update Object assign

--- components/load-more-stories-base.js
+++ components/load-more-stories-base.js
@@ -47,7 +47,7 @@ export class LoadMoreStoriesManager extends React.Component {
 
 export class LoadMoreStoriesBase extends React.Component {
   loadMoreStories(pageNumber) {
-    return getRequest("/api/v1/stories", Object.assign(this.props.params, {
+    return getRequest("/api/v1/stories", Object.assign({}, this.props.params, {
       offset: (this.props.storiesPerPage || 20) * pageNumber,
       fields: this.props.fields
     })).json(response => response.stories || []);
@@ -64,7 +64,7 @@ export class LoadMoreStoriesBase extends React.Component {
 
 export class LoadMoreCollectionStories extends React.Component {
   loadMoreStories(pageNumber) {
-    return getRequest(`/api/v1/collections/${this.props.collectionSlug}`, Object.assign(this.props.params, {
+    return getRequest(`/api/v1/collections/${this.props.collectionSlug}`, Object.assign({}, this.props.params, {
       offset: (this.props.storiesPerPage || 20) * pageNumber
     })).json(response => (response.items || []).map(item => item.story));
   }
