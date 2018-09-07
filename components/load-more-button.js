e71f090a6a998375d4644d137933e21e0054da23
Garima Kamboj <garima.kamboj@icloud.com>
2018-09-07 17:57:10 +0530

    Replace styleName with className

--- components/load-more-button.js
+++ components/load-more-button.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function LoadMoreButton(props) {
-  return <button styleName={props.styleName} onClick={props.onLoadMore}>{props.text}</button>
+  return <button className={props.className} onClick={props.onLoadMore}>{props.text}</button>
 }
 
 LoadMoreButton.propTypes = {
   // CSS class to add styles from  
-  styleName: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
   // Callback function for click event
   onLoadMore: PropTypes.func.isRequired,
   // Button text to display
@@ -15,7 +15,7 @@ LoadMoreButton.propTypes = {
 };
 
 LoadMoreButton.defaultProps = {
-    styleName: 'load-more',
+    className: 'load-more',
     onLoadMore: () => {},
     text: 'Load more',
 }
