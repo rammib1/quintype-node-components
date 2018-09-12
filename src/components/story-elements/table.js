f59dd749b14f789ce547a079fd20fb87cf184584
Tejas Dinkar <tejas@gja.in>
2018-09-13 01:39:23 +0530

    Dynamic Import papaparse

--- src/components/story-elements/table.js
+++ src/components/story-elements/table.js
@@ -1,5 +1,4 @@
 import React from "react";
-import papa from "papaparse";
 
 function TableHeader(columns) {
   return <thead>
@@ -31,10 +30,12 @@ export class Table extends React.Component {
   }
 
   parseCSVToJson(content) {
-    papa.parse(content, {
-      header: this.props.hasHeader,
-      complete: results => this.setState({ tableData: results.data })
-    });
+    import(/* webpackChunkName: "qtc-parsecsv" */ "papaparse").then(({parse}) => {
+      parse(content, {
+        header: this.props.hasHeader,
+        complete: results => this.setState({ tableData: results.data })
+      });
+    })
   }
 
   componentDidMount() {
