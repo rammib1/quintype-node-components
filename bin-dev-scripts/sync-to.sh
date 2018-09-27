e9223b184a7163afcb798a197a9a5c0925109bb4
Asim K T <asim@quintype.com>
2018-09-27 19:56:44 +0530

    Script for automatically build and move library to publisher project folder.

---
+++ bin-dev-scripts/sync-to.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PATH_TO_APP="$1"
+
+if [ -z $PATH_TO_APP ] ; then
+  echo "Please provide relative path to the app; eg  ../chaaipani as the last parameter. Like `npm run sync-files-to ../chaipani/`"
+  exit 1
+fi
+
+npm run build
+
+rsync -r \
+  dist \
+  "$PATH_TO_APP/node_modules/@quintype/components/" \
+  && touch "$PATH_TO_APP/app/isomorphic/pick-component.js"
