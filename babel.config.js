799514f52dceaf8a7ac7855a0a91bbf6748d4bf8
Amith George <strider.am@gmail.com>
2018-12-21 14:01:15 +0530

    feat(build): upgrade to Babel 7 (#55)
    * upgrade to babel 7.1
    
    * fix package-lock after rebasing against master
    
    * Bump dependencies to latest versions

---
+++ babel.config.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = function(api) {
+  const env = api.env();
+  let envOptions = {modules: false};
+  let plugins = [
+    "annotate-pure-calls",
+    "@babel/plugin-syntax-object-rest-spread",
+    "transform-react-remove-prop-types",
+    "@babel/plugin-syntax-dynamic-import"
+  ];
+
+  if (env === 'esmodules') {
+    envOptions = {"targets": {"esmodules": true}};
+  }
+  
+  if (env === 'test') {
+    envOptions = {"targets": {"node": true}};
+    plugins.push("babel-plugin-quintype-assets");
+  }
+
+  let config = {
+    presets: [
+      "@babel/preset-react",
+      [
+        "@babel/preset-env", 
+        envOptions
+      ],
+    ],
+    plugins: plugins
+  };
+  
+  return config;
+}
