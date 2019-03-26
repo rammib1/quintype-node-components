d3140c2fce9e1cd347887c277ab50bbd7c0f8cde
Sinu John <sinujohn91@gmail.com>
2019-03-26 14:49:03 +0530

    refactor code

--- src/components/social-share.js
+++ src/components/social-share.js
@@ -6,18 +6,20 @@ import {withError} from './with-error';
 function SocialShareBase(props) {
   const fullUrl = `${props.publisherUrl}/${props.url}`;
   const hashtags = props.hashtags ? props.hashtags : '';
-  let share = get(global, ["navigator", "share"]);
+  
+  function getShareHandler(title, fullUrl) {
+    const navigatorShare = global.navigator && global.navigator.share;
+    if (!navigatorShare) {
+      return null;
+    }
 
-  function handleShare() {
-    if (share) {
-      share({
-        title: props.title,
+    return function handleShare() {
+      navigatorShare({
+        title: title,
         url: fullUrl,
       });
     }
   }
-
-  let handleShareFn = share ? handleShare : null;
 
   return React.createElement(props.template, Object.assign({
     fbUrl: `https://www.facebook.com/sharer.php?u=${fullUrl}`,
@@ -26,7 +28,7 @@ function SocialShareBase(props) {
     linkedinUrl: `https://www.linkedin.com/shareArticle?url=${fullUrl}&title=${props.title}`,
     whatsappUrl: `https://api.whatsapp.com/send?text=${fullUrl}`,
     mailtoUrl: `mailto:${''}?subject=${props.title}&body=${fullUrl}`,
-    handleShare: handleShareFn
+    handleShare: getShareHandler(props.title, fullUrl)
   }, props));
 }
 
