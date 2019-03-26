3498ff9e4262f5761b4e6f0e6676bb34af3af68b
Sinu John <sinujohn91@gmail.com>
2019-03-26 13:34:54 +0530

    feat(add navigator-share) expose handleShare prop which allows navigator share

--- src/components/social-share.js
+++ src/components/social-share.js
@@ -1,10 +1,23 @@
 import React from "react";
 import {connect} from "react-redux";
+import {get} from "lodash";
 import {withError} from './with-error';
 
 function SocialShareBase(props) {
   const fullUrl = `${props.publisherUrl}/${props.url}`;
   const hashtags = props.hashtags ? props.hashtags : '';
+  let share = get(global, ["navigator", "share"]);
+
+  function handleShare() {
+    if (share) {
+      share({
+        title: props.title,
+        url: fullUrl,
+      });
+    }
+  }
+
+  let handleShareFn = share ? handleShare : null;
 
   return React.createElement(props.template, Object.assign({
     fbUrl: `https://www.facebook.com/sharer.php?u=${fullUrl}`,
@@ -12,7 +25,8 @@ function SocialShareBase(props) {
     gplusUrl: `https://plus.google.com/share?url=${fullUrl}`,
     linkedinUrl: `https://www.linkedin.com/shareArticle?url=${fullUrl}&title=${props.title}`,
     whatsappUrl: `https://api.whatsapp.com/send?text=${fullUrl}`,
-    mailtoUrl: `mailto:${''}?subject=${props.title}&body=${fullUrl}`
+    mailtoUrl: `mailto:${''}?subject=${props.title}&body=${fullUrl}`,
+    handleShare: handleShareFn
   }, props));
 }
 
