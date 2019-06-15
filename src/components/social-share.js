21fc5aa9e2e2e521be3f2feb98ca69cac86fdb23
Garima Kamboj <garima@Garimas-MacBook-Pro.local>
2019-06-15 14:12:32 +0530

    Remove url-encoding from social-share icons

--- src/components/social-share.js
+++ src/components/social-share.js
@@ -32,12 +32,12 @@ class SocialShareBase extends React.Component {
     const hashtags = this.props.hashtags ? this.props.hashtags : '';
 
     return React.createElement(this.props.template, Object.assign({
-      fbUrl: `https://www.facebook.com/sharer.php?u=${encodeURIComponent(fullUrl)}`,
-      twitterUrl: `https://twitter.com/intent/tweet?url=${encodeURIComponent(fullUrl)}&text=${encodeURIComponent(this.props.title)}&hashtags=${hashtags}`,
-      gplusUrl: `https://plus.google.com/share?url=${encodeURIComponent(fullUrl)}`,
-      linkedinUrl: `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(fullUrl)}&title=${encodeURIComponent(this.props.title)}`,
-      whatsappUrl: `https://api.whatsapp.com/send?text=${encodeURIComponent(fullUrl)}`,
-      mailtoUrl: `mailto:${''}?subject=${encodeURIComponent(this.props.title)}&body=${encodeURIComponent(fullUrl)}`,
+      fbUrl: `https://www.facebook.com/sharer.php?u=${fullUrl}`,
+      twitterUrl: `https://twitter.com/intent/tweet?url=${fullUrl}&text=${this.props.title}&hashtags=${hashtags}`,
+      gplusUrl: `https://plus.google.com/share?url=${fullUrl}`,
+      linkedinUrl: `https://www.linkedin.com/shareArticle?url=${fullUrl}&title=${this.props.title}`,
+      whatsappUrl: `https://api.whatsapp.com/send?text=${fullUrl}`,
+      mailtoUrl: `mailto:${''}?subject=${this.props.title}&body=${fullUrl}`,
       handleNativeShare: getNativeShareHandler(this.state.canNativeShare, this.props.title, fullUrl)
     }, this.props));
   }
