c1ff3dbb5bb9be52753de6abf4ea82be4f4795db
Tejas Dinkar <tejas@gja.in>
2020-01-13 10:54:52 +0530

    fix: defaulting mountAt to undefined, to avoid a crash

--- src/components/api-client.js
+++ src/components/api-client.js
@@ -2,14 +2,14 @@ import wretch from 'wretch';
 
 const apiClient = wretch().options({credentials: 'same-origin'});
 
-export function getRequest(url, params, { mountAt = global.qtMountAt || "" }) {
+export function getRequest(url, params, { mountAt = global.qtMountAt || "" } = {}) {
   let client = apiClient.url(`${mountAt}${url}`);
   if(params)
     client = client.query(params);
   return client.get();
 }
 
-export function postRequest(url, body, { mountAt = global.qtMountAt || "" }) {
+export function postRequest(url, body, { mountAt = global.qtMountAt || "" } = {}) {
   return apiClient
     .url(`${mountAt}${url}`)
     .post(body);
