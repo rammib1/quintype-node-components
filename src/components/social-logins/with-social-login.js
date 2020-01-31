d93dad8906541662866b84eca75e65606d49c235
suventure <sharath.nhce@gmail.com>
2020-01-31 20:50:45 +0530

    update sso login path

--- src/components/social-logins/with-social-login.js
+++ src/components/social-logins/with-social-login.js
@@ -49,7 +49,7 @@ export class WithSocialLogin extends React.Component {
   constructor(props) {
     super(props);
     this.serverSideLoginPath = `/login?auth-provider=${this.props.provider}&remote-host=${global.location && global.location.origin}`;
-    this.serverSideSSOLoginPath = `/sso-login?auth-provider=${this.props.provider}&redirectUrl=${this.props.sso && this.props.redirectUrl ? this.props.redirectUrl : global.location && global.location.origin}`;
+    this.serverSideSSOLoginPath = `/login?auth-provider=${this.props.provider}&redirect-url=${this.props.sso && this.props.redirectUrl ? this.props.redirectUrl : global.location && global.location.origin}`;
   }
 
   componentDidMount() {
