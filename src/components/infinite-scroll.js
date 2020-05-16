cdf9c794d2085b63bd1bdd070621e5ed071198be
Ramsharan Gorur Jayaraman <sharangj@gmail.com>
2020-05-16 08:59:43 +0530

    Remove observer disconnect

--- src/components/infinite-scroll.js
+++ src/components/infinite-scroll.js
@@ -5,47 +5,67 @@ class ScrollItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      minHeight: props.minHeight
-    }
+      minHeight: props.minHeight,
+    };
   }
 
   render() {
-    return <div ref={node => this.node = node}
-                data-infinite-scroll={this.props.index}
-                style={{minHeight: this.state.minHeight}}>
-      {this.props.show && this.props.render(Object.assign({index: this.props.index}, this.props.data))}
-    </div>;
+    return (
+      <div
+        ref={(node) => (this.node = node)}
+        data-infinite-scroll={this.props.index}
+        style={{ minHeight: this.state.minHeight }}
+      >
+        {this.props.show &&
+          this.props.render(
+            Object.assign({ index: this.props.index }, this.props.data)
+          )}
+      </div>
+    );
   }
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.show && !this.props.show) {
-      this.setState({minHeight: this.props.minHeight});
-    } else if(!nextProps.show && this.props.show) {
-      this.setState({minHeight: this.node.clientHeight});
+    if (nextProps.show && !this.props.show) {
+      this.setState({ minHeight: this.props.minHeight });
+    } else if (!nextProps.show && this.props.show) {
+      this.setState({ minHeight: this.node.clientHeight });
     }
   }
 
   componentDidMount() {
-    this.props.observers.forEach(observer => observer && observer.observe(this.node));
+    this.props.observers.forEach(
+      (observer) => observer && observer.observe(this.node)
+    );
   }
 
   componentWillUnmount() {
-    this.props.observers.forEach(observer => observer && observer.unobserve(this.node));
+    // this.props.observers.forEach(
+    //   (observer) => observer && observer.unobserve(this.node)
+    // );
   }
 }
 
 // When this becomes visible, we call loadMore()
 class ScrollLoadMore extends React.Component {
   render() {
-    return <div ref={node => this.node = node} data-infinite-scroll="load-more"/>;
+    return (
+      <div
+        ref={(node) => (this.node = node)}
+        data-infinite-scroll="load-more"
+      />
+    );
   }
 
   componentDidMount() {
-    this.props.observers.forEach(observer => observer && observer.observe(this.node));
+    this.props.observers.forEach(
+      (observer) => observer && observer.observe(this.node)
+    );
   }
 
   componentWillUnmount() {
-    this.props.observers.forEach(observer => observer && observer.unobserve(this.node));
+    // this.props.observers.forEach(
+    //   (observer) => observer && observer.unobserve(this.node)
+    // );
   }
 }
 
@@ -55,26 +75,31 @@ class InfiniteScrollBase extends React.Component {
     super(props);
     const initiallyShow = props.initiallyShow || 1;
     this.state = {
-      visibleComponents: [...Array(initiallyShow).keys()].reduce((arr, i) => { arr[i] = true; return arr }, {})
-    }
-    if(global.IntersectionObserver) {
-      this.loadObserver = new IntersectionObserver((x) => this.intersectionCallback(x), {
-        rootMargin: props.loadMargin || "200px 0px 500px"
-      });
+      visibleComponents: [...Array(initiallyShow).keys()].reduce((arr, i) => {
+        arr[i] = true;
+        return arr;
+      }, {}),
+    };
+    if (global.IntersectionObserver) {
+      this.loadObserver = new IntersectionObserver(
+        (x) => this.intersectionCallback(x),
+        {
+          rootMargin: props.loadMargin || "200px 0px 500px",
+        }
+      );
     }
   }
 
   componentWillUnmount() {
-    this.loadObserver && this.loadObserver.disconnect();
+    //this.loadObserver && this.loadObserver.disconnect();
   }
 
   intersectionCallback(entries) {
     var visibleComponents = this.state.visibleComponents;
-    entries.forEach(entry => {
-
+    entries.forEach((entry) => {
       // Stupid browsers like UC and Mi don't correctly support the spec
-      if(entry.isIntersecting === undefined) {
-        if(this.props.showAllOnLegacyBrowser) {
+      if (entry.isIntersecting === undefined) {
+        if (this.props.showAllOnLegacyBrowser) {
           entry.isIntersecting = true;
         } else {
           return;
@@ -82,28 +107,37 @@ class InfiniteScrollBase extends React.Component {
       }
 
       const item = entry.target.getAttribute("data-infinite-scroll");
-      if(item == 'load-more' && entry.isIntersecting) {
+      if (item == "load-more" && entry.isIntersecting) {
         this.props.loadNext();
       } else {
-        const showItem = this.props.neverHideItem ? visibleComponents[item] || entry.isIntersecting : entry.isIntersecting;
-        visibleComponents = Object.assign({}, visibleComponents, {[item]: showItem});
+        const showItem = this.props.neverHideItem
+          ? visibleComponents[item] || entry.isIntersecting
+          : entry.isIntersecting;
+        visibleComponents = Object.assign({}, visibleComponents, {
+          [item]: showItem,
+        });
       }
-    })
-    this.setState({visibleComponents: visibleComponents});
+    });
+    this.setState({ visibleComponents: visibleComponents });
   }
 
   render() {
-    return <div>
-      {this.props.items.map((data, index) =>
-        <ScrollItem observers={this.props.observers.concat([this.loadObserver])}
-                    key={index}
-                    index={index}
-                    show={this.state.visibleComponents[index]}
-                    render={this.props.render}
-                    data={data}
-                    minHeight={this.props.minHeight || 50}/>)}
-      <ScrollLoadMore observers={[this.loadObserver]} />
-    </div>;
+    return (
+      <div>
+        {this.props.items.map((data, index) => (
+          <ScrollItem
+            observers={this.props.observers.concat([this.loadObserver])}
+            key={index}
+            index={index}
+            show={this.state.visibleComponents[index]}
+            render={this.props.render}
+            data={data}
+            minHeight={this.props.minHeight || 50}
+          />
+        ))}
+        <ScrollLoadMore observers={[this.loadObserver]} />
+      </div>
+    );
   }
 }
 
@@ -112,32 +146,40 @@ function withFocusObserver(Component) {
   return class WithFocusObserver extends React.Component {
     constructor(props) {
       super(props);
-      if(global.IntersectionObserver) {
-        this.focusObserver = new IntersectionObserver((x) => this.focusCallback(x), {
-          rootMargin: `-${100 - props.focusCallbackAt}% 0px -${props.focusCallbackAt}%`
-        })
+      if (global.IntersectionObserver) {
+        this.focusObserver = new IntersectionObserver(
+          (x) => this.focusCallback(x),
+          {
+            rootMargin: `-${100 - props.focusCallbackAt}% 0px -${
+              props.focusCallbackAt
+            }%`,
+          }
+        );
       }
     }
 
     componentWillUnmount() {
-      this.focusObserver && this.focusObserver.disconnect();
+      //this.focusObserver && this.focusObserver.disconnect();
     }
 
     focusCallback(entries) {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         const item = entry.target.getAttribute("data-infinite-scroll");
-        if(entry.isIntersecting) {
-          this.props.onFocus(item)
+        if (entry.isIntersecting) {
+          this.props.onFocus(item);
         }
       });
     }
 
     render() {
-      return React.createElement(Component, Object.assign({}, this.props, {
-        observers: (this.props.observers || []).concat([this.focusObserver])
-      }))
+      return React.createElement(
+        Component,
+        Object.assign({}, this.props, {
+          observers: (this.props.observers || []).concat([this.focusObserver]),
+        })
+      );
     }
-  }
+  };
 }
 
 /**
