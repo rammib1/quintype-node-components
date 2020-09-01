ab8ee0e2fc61c7eb0f5c471411d7b2c4c8ea3de4
Sivaram Balakrishnan <sivaram.balakrishnan@quintype.com>
2020-09-01 13:52:23 +0530

    Revert "Updates"
    This reverts commit b91e658d5053b2d8625446625f71ce0bbb5e27c8.

--- entry.js
+++ entry.js
@@ -1,131 +1,123 @@
 
-    window.reactComponents = {};
+    import React from "react";
 
-    window.vueComponents = {};
+    import ReactDOM from "react-dom";
 
-  
-      import React from "react";
-
-      import ReactDOM from "react-dom";
+    window.Components = {};
 
 
-      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+    import Wrapper from '../node_modules/better-docs/lib/wrapper.js';
 
-      window.React = React;
+    window.React = React;
 
-      window.ReactDOM = ReactDOM;
+    window.ReactDOM = ReactDOM;
 
-      window.ReactWrapper = ReactWrapper;
+    window.Wrapper = Wrapper;
 
-    
-    import './styles/reset.css';
+  import AccessTypeBase from '../src/components/access-type.js';
+Components.AccessTypeBase = AccessTypeBase;
 
-    import './styles/iframe.css';
+import AdbutlerAd from '../src/components/adbutler-ad.js';
+Components.AdbutlerAd = AdbutlerAd;
 
-  import Component0 from '../src/components/access-type.js';
-reactComponents['AccessTypeBase'] = Component0;
+import BreakingNewsBase from '../src/components/breaking-news.js';
+Components.BreakingNewsBase = BreakingNewsBase;
 
-import Component1 from '../src/components/adbutler-ad.js';
-reactComponents['AdbutlerAd'] = Component1;
+import BreakingNewsItem from '../src/components/breaking-news-item.js';
+Components.BreakingNewsItem = BreakingNewsItem;
 
-import Component2 from '../src/components/breaking-news.js';
-reactComponents['BreakingNewsBase'] = Component2;
+import ClientSideOnlyBase from '../src/components/client-side-only.js';
+Components.ClientSideOnlyBase = ClientSideOnlyBase;
 
-import Component3 from '../src/components/breaking-news-item.js';
-reactComponents['BreakingNewsItem'] = Component3;
+import Collection from '../src/components/collection.js';
+Components.Collection = Collection;
 
-import Component4 from '../src/components/client-side-only.js';
-reactComponents['ClientSideOnlyBase'] = Component4;
+import EagerLoadImages from '../src/components/eager-load-images.js';
+Components.EagerLoadImages = EagerLoadImages;
 
-import Component5 from '../src/components/collection.js';
-reactComponents['Collection'] = Component5;
+import HamburgerButtonBase from '../src/components/hamburger-button.js';
+Components.HamburgerButtonBase = HamburgerButtonBase;
 
-import Component6 from '../src/components/eager-load-images.js';
-reactComponents['EagerLoadImages'] = Component6;
+import ImageGalleryElement from '../src/components/image-gallery.js';
+Components.ImageGalleryElement = ImageGalleryElement;
 
-import Component7 from '../src/components/hamburger-button.js';
-reactComponents['HamburgerButtonBase'] = Component7;
+import InfiniteStoryBase from '../src/components/infinite-story-base.js';
+Components.InfiniteStoryBase = InfiniteStoryBase;
 
-import Component8 from '../src/components/image-gallery.js';
-reactComponents['ImageGalleryElement'] = Component8;
+import LazyCollection from '../src/components/lazy-collection.js';
+Components.LazyCollection = LazyCollection;
 
-import Component9 from '../src/components/infinite-story-base.js';
-reactComponents['InfiniteStoryBase'] = Component9;
+import LazyLoadImages from '../src/components/lazy-load-images.js';
+Components.LazyLoadImages = LazyLoadImages;
 
-import Component10 from '../src/components/lazy-collection.js';
-reactComponents['LazyCollection'] = Component10;
+import LinkBase from '../src/components/link.js';
+Components.LinkBase = LinkBase;
 
-import Component11 from '../src/components/lazy-load-images.js';
-reactComponents['LazyLoadImages'] = Component11;
+import LoadingIndicatorBase from '../src/components/loading-indicator.js';
+Components.LoadingIndicatorBase = LoadingIndicatorBase;
 
-import Component12 from '../src/components/link.js';
-reactComponents['LinkBase'] = Component12;
+import LoadMoreCollectionStories from '../src/components/load-more-collection-stories.js';
+Components.LoadMoreCollectionStories = LoadMoreCollectionStories;
 
-import Component13 from '../src/components/loading-indicator.js';
-reactComponents['LoadingIndicatorBase'] = Component13;
+import LoadMoreStoriesBase from '../src/components/load-more-stories-base.js';
+Components.LoadMoreStoriesBase = LoadMoreStoriesBase;
 
-import Component14 from '../src/components/load-more-collection-stories.js';
-reactComponents['LoadMoreCollectionStories'] = Component14;
+import MenuBase from '../src/components/menu.js';
+Components.MenuBase = MenuBase;
 
-import Component15 from '../src/components/load-more-stories-base.js';
-reactComponents['LoadMoreStoriesBase'] = Component15;
+import MenuItem from '../src/components/menu-item.js';
+Components.MenuItem = MenuItem;
 
-import Component16 from '../src/components/menu.js';
-reactComponents['MenuBase'] = Component16;
+import ResponsiveHeroImage from '../src/components/responsive-hero-image.js';
+Components.ResponsiveHeroImage = ResponsiveHeroImage;
 
-import Component17 from '../src/components/menu-item.js';
-reactComponents['MenuItem'] = Component17;
+import ResponsiveImageBase from '../src/components/responsive-image.js';
+Components.ResponsiveImageBase = ResponsiveImageBase;
 
-import Component18 from '../src/components/responsive-hero-image.js';
-reactComponents['ResponsiveHeroImage'] = Component18;
+import ResponsiveSource from '../src/components/responsive-source.js';
+Components.ResponsiveSource = ResponsiveSource;
 
-import Component19 from '../src/components/responsive-image.js';
-reactComponents['ResponsiveImageBase'] = Component19;
+import ReviewRating from '../src/components/review-rating/review-rating.js';
+Components.ReviewRating = ReviewRating;
 
-import Component20 from '../src/components/responsive-source.js';
-reactComponents['ResponsiveSource'] = Component20;
+import SearchBoxBase from '../src/components/search-box.js';
+Components.SearchBoxBase = SearchBoxBase;
 
-import Component21 from '../src/components/review-rating/review-rating.js';
-reactComponents['ReviewRating'] = Component21;
+import SearchPageBase from '../src/components/search-page-base.js';
+Components.SearchPageBase = SearchPageBase;
 
-import Component22 from '../src/components/search-box.js';
-reactComponents['SearchBoxBase'] = Component22;
+import SocialShareBase from '../src/components/social-share.js';
+Components.SocialShareBase = SocialShareBase;
 
-import Component23 from '../src/components/search-page-base.js';
-reactComponents['SearchPageBase'] = Component23;
+import StoryElement from '../src/components/story-element.js';
+Components.StoryElement = StoryElement;
 
-import Component24 from '../src/components/social-share.js';
-reactComponents['SocialShareBase'] = Component24;
+import UpdateOnInterval from '../src/components/update-on-interval.js';
+Components.UpdateOnInterval = UpdateOnInterval;
 
-import Component25 from '../src/components/story-element.js';
-reactComponents['StoryElement'] = Component25;
+import WithClientSideOnly from '../src/components/with-client-side-only.js';
+Components.WithClientSideOnly = WithClientSideOnly;
 
-import Component26 from '../src/components/update-on-interval.js';
-reactComponents['UpdateOnInterval'] = Component26;
+import WithFacebookLogin from '../src/components/social-logins/with-facebook-login.js';
+Components.WithFacebookLogin = WithFacebookLogin;
 
-import Component27 from '../src/components/with-client-side-only.js';
-reactComponents['WithClientSideOnly'] = Component27;
+import WithGoogleLogin from '../src/components/social-logins/with-google-login.js';
+Components.WithGoogleLogin = WithGoogleLogin;
 
-import Component28 from '../src/components/social-logins/with-facebook-login.js';
-reactComponents['WithFacebookLogin'] = Component28;
+import WithHostUrl from '../src/components/with-host-url.js';
+Components.WithHostUrl = WithHostUrl;
 
-import Component29 from '../src/components/social-logins/with-google-login.js';
-reactComponents['WithGoogleLogin'] = Component29;
+import WithLazy from '../src/components/with-lazy.js';
+Components.WithLazy = WithLazy;
 
-import Component30 from '../src/components/with-host-url.js';
-reactComponents['WithHostUrl'] = Component30;
+import WithLinkedInLogin from '../src/components/social-logins/with-linkedin-login.js';
+Components.WithLinkedInLogin = WithLinkedInLogin;
 
-import Component31 from '../src/components/with-lazy.js';
-reactComponents['WithLazy'] = Component31;
+import WithMemberBase from '../src/components/with-member.js';
+Components.WithMemberBase = WithMemberBase;
 
-import Component32 from '../src/components/social-logins/with-linkedin-login.js';
-reactComponents['WithLinkedInLogin'] = Component32;
+import WithSocialLogin from '../src/components/social-logins/with-social-login.js';
+Components.WithSocialLogin = WithSocialLogin;
 
-import Component33 from '../src/components/with-member.js';
-reactComponents['WithMemberBase'] = Component33;
-
-import Component34 from '../src/components/social-logins/with-social-login.js';
-reactComponents['WithSocialLogin'] = Component34;
-
-import Component35 from '../src/components/social-logins/with-twitter-login.js';
-reactComponents['WithTwitterLogin'] = Component35;
+import WithTwitterLogin from '../src/components/social-logins/with-twitter-login.js';
+Components.WithTwitterLogin = WithTwitterLogin;
