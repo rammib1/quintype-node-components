ba9d1c0045beb76e174e9a77810ed6dc09956764
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2020-10-12 12:27:07 +0530

    wip

--- src/components/story-element.js
+++ src/components/story-element.js
@@ -1,13 +1,13 @@
-import React from "react";
-import classNames from "classnames";
-import JSEmbed from "./story-elements/jsembed";
-import StoryElementYoutube from "./story-elements/youtube";
-import { ResponsiveImage } from "./responsive-image";
-import Polltype from "./story-elements/polltype";
-import { Table } from "./story-elements/table";
-import { Link } from "./link";
-import get from "lodash/get";
-import { getQliticsSchema } from "../utils";
+import React from 'react';
+import classNames from 'classnames';
+import JSEmbed from './story-elements/jsembed';
+import StoryElementYoutube from './story-elements/youtube';
+import {ResponsiveImage} from './responsive-image';
+import Polltype from './story-elements/polltype';
+import {Table} from './story-elements/table';
+import {Link} from './link';
+import get from 'lodash/get';
+import {getQliticsSchema} from '../utils';
 
 /**
  * This component renders different types of [story elements](https://developers.quintype.com/malibu/terminology#story-elements)
@@ -37,12 +37,12 @@ import { getQliticsSchema } from "../utils";
 export class StoryElement extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { crashed: false };
+    this.state = {crashed: false};
   }
   componentDidCatch(error, stack) {
     this.setState({
       crashed: true,
-      error: error
+      error: error,
     });
     console && console.error(`Caught Exception: ${error && error.message}`);
   }
@@ -56,32 +56,32 @@ export class StoryElement extends React.Component {
   }
 }
 
-function StoryElementText({ element = {}, externalLink }) {
-  let text = element.text || "";
+function StoryElementText({element = {}, externalLink}) {
+  let text = element.text || '';
   if (externalLink) {
     text = element.text.replace(/<a/g, '<a target="_blank"');
   }
-  return React.createElement("div", {
-    dangerouslySetInnerHTML: { __html: text }
+  return React.createElement('div', {
+    dangerouslySetInnerHTML: {__html: text},
   });
 }
 
-function StoryElementAlsoRead({ element, story, alsoreadText = "Also Read: " }) {
-  const linkedStories = get(story, ["linked-stories"]);
-  const linkedStoryId = get(element, ["metadata", "linked-story-id"]);
-  const linkedStorySlug = get(linkedStories, [linkedStoryId, "slug"]);
+function StoryElementAlsoRead({element, story, alsoreadText = 'Also Read: '}) {
+  const linkedStories = get(story, ['linked-stories']);
+  const linkedStoryId = get(element, ['metadata', 'linked-story-id']);
+  const linkedStorySlug = get(linkedStories, [linkedStoryId, 'slug']);
   const storyUrl = `/${linkedStorySlug}`;
   const linkProps = {
-    className: "story-element-text-also-read__link",
-    href: storyUrl
+    className: 'story-element-text-also-read__link',
+    href: storyUrl,
   };
 
   return React.createElement(
-    "h3",
+    'h3',
     {},
     React.createElement(
-      "span",
-      { className: "story-element-text-also-read__label" },
+      'span',
+      {className: 'story-element-text-also-read__label'},
       alsoreadText
     ),
     React.createElement(Link, linkProps, element.text)
@@ -93,87 +93,88 @@ function StoryElementImage({
   story = {},
   imageWidths,
   imageDefaultWidth,
-  onClick = () => {}
+  onClick = () => {},
 }) {
+  console.log('ResponsiveImage####', ResponsiveImage);
   return React.createElement(
-    "figure",
+    'figure',
     {},
     React.createElement(ResponsiveImage, {
-      slug: element["image-s3-key"],
-      metadata: element["metadata"],
+      slug: element['image-s3-key'],
+      metadata: element['metadata'],
       aspectRatio: null,
       defaultWidth: imageDefaultWidth || 640,
       widths: imageWidths || [360, 640, 1200],
-      imgParams: { auto: ["format", "compress"] },
+      imgParams: {auto: ['format', 'compress']},
       alt: element.title || story.headline,
-      onClick
+      onClick,
     }),
     React.createElement(
-      "div",
-      { className: "story-element-caption-attribution-wrapper" },
+      'div',
+      {className: 'story-element-caption-attribution-wrapper'},
       element.title
-        ? React.createElement("figcaption", {
-            dangerouslySetInnerHTML: { __html: element.title },
-            className: "story-element-image-title"
+        ? React.createElement('figcaption', {
+            dangerouslySetInnerHTML: {__html: element.title},
+            className: 'story-element-image-title',
           })
         : undefined,
-      element["image-attribution"]
-        ? React.createElement("figcaption", {
-            dangerouslySetInnerHTML: { __html: element["image-attribution"] },
-            className: "story-element-image-attribution"
+      element['image-attribution']
+        ? React.createElement('figcaption', {
+            dangerouslySetInnerHTML: {__html: element['image-attribution']},
+            className: 'story-element-image-attribution',
           })
         : undefined
     )
   );
 }
 
-function StoryElementTitle({ element }) {
-  return React.createElement("h2", {}, element.text);
+function StoryElementTitle({element}) {
+  return React.createElement('h2', {}, element.text);
 }
 
-function StoryElementSoundCloud({ element }) {
-  return React.createElement("iframe", {
-    src: element["embed-url"],
-    width: "100%"
+function StoryElementSoundCloud({element}) {
+  return React.createElement('iframe', {
+    src: element['embed-url'],
+    width: '100%',
   });
 }
 
-function StoryElementJsembed({ element }) {
+function StoryElementJsembed({element}) {
   return React.createElement(JSEmbed, {
-    embedJS: element["embed-js"],
-    id: element["id"]
+    embedJS: element['embed-js'],
+    id: element['id'],
   });
 }
 
-function StoryElementPolltype({ element }) {
-  return React.createElement(Polltype, { id: element["polltype-id"] });
+function StoryElementPolltype({element}) {
+  return React.createElement(Polltype, {id: element['polltype-id']});
 }
 
-function StoryElementTable({ element }) {
+function StoryElementTable({element}) {
   return React.createElement(Table, {
     id: element.id,
     data: element.data,
-    hasHeader: element.metadata["has-header"]
+    hasHeader: element.metadata['has-header'],
   });
 }
 
-function StoryElementFile({ element }) {
+function StoryElementFile({element}) {
   return React.createElement(
     React.Fragment,
     null,
     React.createElement(
-      "div",
-      { className: "story-element-file__title" },
-      element["file-name"]
+      'div',
+      {className: 'story-element-file__title'},
+      element['file-name']
     ),
     React.createElement(
-      "a",
+      'a',
       {
-        className: "story-element-file__link",
+        className: 'story-element-file__link',
         href: element.url,
-        download: true
+        download: true,
       },
-      "download"
+      'download'
     )
   );
 }
@@ -185,13 +186,13 @@ const DEFAULT_TEMPLATES = {
   text: StoryElementText,
   image: StoryElementImage,
   title: StoryElementTitle,
-  "youtube-video": StoryElementYoutube,
-  "soundcloud-audio": StoryElementSoundCloud,
+  'youtube-video': StoryElementYoutube,
+  'soundcloud-audio': StoryElementSoundCloud,
   jsembed: StoryElementJsembed,
   polltype: StoryElementPolltype,
   table: StoryElementTable,
-  "also-read": StoryElementAlsoRead,
-  file: StoryElementFile
+  'also-read': StoryElementAlsoRead,
+  file: StoryElementFile,
 };
 
 class StoryElementBase extends React.Component {
@@ -214,8 +215,8 @@ class StoryElementBase extends React.Component {
 
     const options = {
       root: null,
-      rootMargin: "0px",
-      threshold: 1.0
+      rootMargin: '0px',
+      threshold: 1.0,
     };
     this.observer = new IntersectionObserver(this.observerCallback, options);
     this.observer.observe(this.storyElementRef);
@@ -241,16 +242,16 @@ class StoryElementBase extends React.Component {
       this.props.templates
     );
     return (
-      templates[storyElement.subtype] || templates[storyElement.type] || "div"
+      templates[storyElement.subtype] || templates[storyElement.type] || 'div'
     );
   }
 
   emitElementQlitics() {
-    const { story = {}, card = {}, element = {} } = this.props;
+    const {story = {}, card = {}, element = {}} = this.props;
     if (global.qlitics) {
       global.qlitics(
-        "track",
-        "story-element-view",
+        'track',
+        'story-element-view',
         getQliticsSchema(story, card, element)
       );
     } else {
@@ -260,8 +261,8 @@ class StoryElementBase extends React.Component {
           (qlitics.q = qlitics.q || []).push(arguments);
         };
       global.qlitics(
-        "track",
-        "story-element-view",
+        'track',
+        'story-element-view',
         getQliticsSchema(story, card, element)
       );
     }
@@ -274,31 +275,29 @@ class StoryElementBase extends React.Component {
   render() {
     const storyElement = this.props.element;
     const typeClassName = `story-element-${storyElement.type}`;
-    const subtypeClassName = `story-element-${storyElement.type}-${
-      storyElement.subtype
-    }`;
+    const subtypeClassName = `story-element-${storyElement.type}-${storyElement.subtype}`;
 
     const storyElementTemplate = this.template();
 
-    const { renderTemplates = {}, ...elementProps } = this.props;
+    const {renderTemplates = {}, ...elementProps} = this.props;
     const renderTemplate =
       renderTemplates[storyElement.subtype] ||
       renderTemplates[storyElement.type];
 
     return React.createElement(
-      "div",
+      'div',
       {
         className: classNames({
-          "story-element": true,
+          'story-element': true,
           [typeClassName]: true,
-          [subtypeClassName]: !!storyElement.subtype
+          [subtypeClassName]: !!storyElement.subtype,
         }),
-        ref: ref => (this.storyElementRef = ref)
+        ref: ref => (this.storyElementRef = ref),
       },
       renderTemplate
         ? React.createElement(
             renderTemplate,
-            { ...elementProps },
+            {...elementProps},
             React.createElement(
               storyElementTemplate,
               Object.assign({}, elementProps)
