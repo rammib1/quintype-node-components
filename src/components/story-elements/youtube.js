e869ddd91735c7844795f97a5f639c9812f1d967
Harshith46 <45616022+Harshith46@users.noreply.github.com>
2020-11-06 18:37:56 +0530

    feat: add a toggle to load the youtube iframe on click of thumbnail (#158)
    * feat: add a toggle to load the youtube iframe on click of thumbnail
    
    * code ref
    
    * chore(release): 2.23.0-youtube-render.0
    
    * chore(release): 2.23.0-youtube-render.1
    
    * default playback after click of button
    
    * chore(release): 2.23.0-youtube-render.2
    
    * render both thumbnail and iframe on click for better UI
    
    * chore(release): 2.23.0-youtube-render.3
    
    * remove the automatic video playing when default
    
    * update package
    
    Co-authored-by: Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>

--- src/components/story-elements/youtube.js
+++ src/components/story-elements/youtube.js
@@ -1,40 +1,48 @@
-import React from "react";
+import React from 'react';
 import getYouTubeID from 'get-youtube-id';
-import {getQliticsSchema} from "../../utils";
+import {getQliticsSchema} from '../../utils';
 
 let YouTube = null;
 let loaderPromise = null;
 
-function loadLibrary(){
+function loadLibrary() {
   if (loaderPromise === null) {
-    loaderPromise = import(/* webpackChunkName: "qtc-react-youtube" */ "react-youtube").then(YT => {
-      YouTube = YT.default;
-    }).catch(err => {
-      console.log('Failed to load react-youtube', err);
-      return Promise.reject();
-    });
+    loaderPromise = import(
+      /* webpackChunkName: "qtc-react-youtube" */ 'react-youtube'
+    )
+      .then(YT => {
+        YouTube = YT.default;
+      })
+      .catch(err => {
+        console.log('Failed to load react-youtube', err);
+        return Promise.reject();
+      });
   }
 
   return loaderPromise;
 }
 
-function isLibraryLoaded(){
+function isLibraryLoaded() {
   return YouTube !== null;
 }
 
 export default class StoryElementYoutube extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showVideo: false,
+    };
     this.opts = {
       playerVars: {
-        autoplay: 0
-      }
+        autoplay: 0,
+      },
     };
   }
 
-  componentDidMount(){
-    this._isMounted = true;
-    loadLibrary().then(() => this._isMounted && this.forceUpdate());
+  componentDidMount() {
+    if (!this.props.loadIframeOnClick) {
+      this.triggerIframe();
+    }
   }
 
   componentWillUnmount() {
@@ -42,44 +50,96 @@ export default class StoryElementYoutube extends React.Component {
   }
 
   triggerQlitics = action => {
-    if(this.props.disableAnalytics === true) return false;
-    
+    if (this.props.disableAnalytics === true) return false;
+
     const {story = {}, card = {}, element = {}} = this.props;
-    const qliticsData = {...getQliticsSchema(story,card,element), ...{'story-element-action': action}};
-    if(global.qlitics){
+    const qliticsData = {
+      ...getQliticsSchema(story, card, element),
+      ...{'story-element-action': action},
+    };
+    if (global.qlitics) {
       global.qlitics('track', 'story-element-action', qliticsData);
-    }else{
-      global.qlitics=global.qlitics||function(){(qlitics.q=qlitics.q||[]).push(arguments);};
+    } else {
+      global.qlitics =
+        global.qlitics ||
+        function() {
+          (qlitics.q = qlitics.q || []).push(arguments);
+        };
       qlitics.qlitics.q.push('track', 'story-element-action', qliticsData);
     }
   };
 
   onPlayCallback = event => {
     this.triggerQlitics('play');
-    this.props.onPlay === "function" && this.props.onPlay(event);
+    this.props.onPlay === 'function' && this.props.onPlay(event);
   };
 
   onPauseCallback = event => {
     this.triggerQlitics('pause');
-    this.props.onPause === "function" && this.props.onPause(event);
+    this.props.onPause === 'function' && this.props.onPause(event);
   };
 
   onEndCallback = event => {
     this.triggerQlitics('end');
-    this.props.onEnd === "function" && this.props.onEnd(event);
+    this.props.onEnd === 'function' && this.props.onEnd(event);
+  };
+
+  triggerIframe = () => {
+    this._isMounted = true;
+    loadLibrary().then(() => this._isMounted && this.forceUpdate());
+  };
+
+  onPlayerReady = event => {
+    event.target.setVolume(100);
+    event.target.playVideo();
+  };
+
+  renderVideo = () => {
+    this.triggerIframe();
+    this.setState({showVideo: true});
   };
 
   render() {
-    if (isLibraryLoaded()) {
+    const youtubeIframe = () => {
       return React.createElement(YouTube, {
         videoId: getYouTubeID(this.props.element.url),
-        opts:this.opts,
+        opts: this.opts,
         onPlay: this.onPlayCallback,
         onPause: this.onPauseCallback,
-        onEnd: this.onEndCallback
+        onEnd: this.onEndCallback,
+        onReady: this.onPlayerReady,
       });
-    }
+    };
 
-    return <div></div>;
+    if (this.props.loadIframeOnClick) {
+      return (
+        <div className="thumbnail-wrapper">
+          <button
+            className="youtube-playBtn"
+            onClick={this.renderVideo}
+            aria-label="Play Video"
+          />
+          <img
+            className="youtube-thumbnail"
+            onClick={this.renderVideo}
+            src={`https://img.youtube.com/vi/${getYouTubeID(
+              this.props.element.url
+            )}/sddefault.jpg`}
+            alt="video"
+          />
+          {this.state.showVideo && isLibraryLoaded() && (
+            <div className="youtube-iframe-wrapper">{youtubeIframe()}</div>
+          )}
+        </div>
+      );
+    } else if (!this.props.loadIframeOnClick && isLibraryLoaded()) {
+      return React.createElement(YouTube, {
+        videoId: getYouTubeID(this.props.element.url),
+        opts: this.opts,
+        onPlay: this.onPlayCallback,
+        onPause: this.onPauseCallback,
+        onEnd: this.onEndCallback,
+      });
+    } else return <div></div>;
   }
 }
